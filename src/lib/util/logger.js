6e8a3d372539c35914dcb838e8458aceeccadb32
Ali Ok <aliok@apache.org>
2018-10-22 16:59:43 +0300

    feat: Have client info in audit logs AEROGEAR-8024

--- src/lib/util/logger.js
+++ src/lib/util/logger.js
@@ -6,18 +6,36 @@ const auditLogEnabled = process.env.AUDIT_LOGGING !== 'false' && process.env.AUD
 function auditLog (success, request, info, parent, args, msg) {
   if (auditLogEnabled) {
     auditLogger.info({
-      msg: msg || '',
-      requestId: request ? request.id : '',
-      operationType: info.operation.operation,
-      fieldName: info.fieldname,
-      parentTypeName: info.parentType.name,
-      path: buildPath(info.path),
-      success: success,
-      parent: parent,
-      arguments: args,
-      dataSourceType: info.dataSourceType || ''
+      audit: {
+        msg: msg || '',
+        requestId: request ? request.id : '',
+        operationType: info.operation.operation,
+        fieldName: info.fieldname,
+        parentTypeName: info.parentType.name,
+        path: buildPath(info.path),
+        success: success,
+        parent: parent,
+        arguments: args,
+        dataSourceType: info.dataSourceType || '',
+        clientInfo: getClientInfoFromHeaders(request)
+      }
     })
   }
+}
+
+function getClientInfoFromHeaders (request) {
+  if (request && request.headers && request.headers['data-sync-client-info']) {
+    const encoded = request.headers['data-sync-client-info']
+    const buf = Buffer.from(encoded, 'base64')
+    const decoded = buf.toString('utf8')
+    try {
+      return JSON.parse(decoded)
+    } catch (e) {
+      log.error('Unable to parse client-info header provided by the client. Message: ' + e.message)
+      return undefined
+    }
+  }
+  return undefined
 }
 
 // builds path for a GraphQL operation
